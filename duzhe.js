74400bf541a66bbbe6e675cc87969d9a6f73c122
Dearsiwen <93439128+Dearsiwen@users.noreply.github.com>
2021-10-31 17:29:34 +0800

    Update duzhe.js

--- duzhe.js
+++ duzhe.js
@@ -1,35 +1,3 @@
-/*
-**********************
-QuantumultX :
-**********************
-[rewrite_local]
-# Remove ADs
-^(http|https):\/\/[\w-]+\.fengread\.com\/tuwen\/appads\/ url reject-200
-https?:\/\/www\.fengread\.com\/(tuwen\/apktw\/bookv5|ebookreader\/fyhuserv5\/userinfo) url script-response-body https://raw.githubusercontent.com/Dearsiwen/surge/main/duzhe.js
-[mitm] 
-hostname= www.fengread.com
-
-**********************
-Surge 4.2.0+ :
-**********************
-[URL Rewrite]
-# Remove ADs
-^(http|https):\/\/[\w-]+\.fengread\.com\/tuwen\/appads\/ - reject
-[Script]
-duzhe = type=http-response,pattern=https?:\/\/www\.fengread\.com\/(tuwen\/apktw\/bookv5|ebookreader\/fyhuserv5\/userinfo),script-path=https://raw.githubusercontent.com/Dearsiwen/surge/main/duzhe.js, requires-body=true, timeout=10
-[mitm] 
-hostname= www.fengread.com
-
-************************
-Loon 2.1.0+ :
-************************
-[URL Rewrite]
-# Remove ADs
-^(http|https):\/\/[\w-]+\.fengread\.com\/tuwen\/appads\/ - reject
-[Script]
-http-response https?:\/\/www\.fengread\.com\/(tuwen\/apktw\/bookv5|ebookreader\/fyhuserv5\/userinfo) script-path=https://raw.githubusercontent.com/Dearsiwen/surge/main/duzhe.js, requires-body=true, timeout=10, tag=duzhe
-[mitm] 
-hostname= www.fengread.com
 
 */
 var body = $response.body;
