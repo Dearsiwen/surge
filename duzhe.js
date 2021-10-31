c7f822c9f6f0135ff576f9be27c6020447eac4d5
Dearsiwen <93439128+Dearsiwen@users.noreply.github.com>
2021-10-31 17:39:33 +0800

    Update duzhe.js

--- duzhe.js
+++ duzhe.js
@@ -1,5 +1,3 @@
-
-*/
 var body = $response.body;
 body=body.replace(/price\":\"(.*?)\"/g,'price":\"0\"').replace(/isvip\":\"(.*?)\"/g,'isvip":\"1\"').replace(/vip\_type\":\"(.*?)\"/g,'vip\_type":\"1\"');
 $done({body});
